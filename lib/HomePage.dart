71a55ae8ce10eb5b3f53ae6dd6da9f3575856d65
Swapnil Yadav <46942236+yadavswap@users.noreply.github.com>
2023-10-03 18:43:34 +0530

    finish logic

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -9,7 +9,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
   AssetImage one = AssetImage("images/one.png");
   AssetImage two = AssetImage("images/two.png");
   AssetImage three = AssetImage("images/three.png");
@@ -17,7 +16,7 @@ class _HomePageState extends State<HomePage> {
   AssetImage five = AssetImage("images/five.png");
   AssetImage six = AssetImage("images/six.png");
 
-  AssetImage diceimage;
+  late AssetImage diceimage;
 
   @override
   void initState() {
@@ -29,8 +28,7 @@ class _HomePageState extends State<HomePage> {
 
   void rollDice() {
     int random = (1 + Random().nextInt(6));
-
-    AssetImage newImage;
+    AssetImage? newImage;
 
     switch (random) {
       case 1:
@@ -51,16 +49,40 @@ class _HomePageState extends State<HomePage> {
       case 6:
         newImage = six;
         break;
-      default:
     }
 
     setState(() {
-      diceimage = newImage;
-      
+      diceimage = newImage ?? one; // Set diceimage to the newly rolled image
     });
   }
 
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dice Roller'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: diceimage,
+                width: 200.0,
+                height: 200.0,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 100.0),
+                child: ElevatedButton(
+                  onPressed: rollDice,
+                  child: Text('Roll the Dice !'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
