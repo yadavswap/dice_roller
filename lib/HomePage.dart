5dd11ae5986dae1aa55d3d0719611d9d89cca740
Swapnil Yadav <46942236+yadavswap@users.noreply.github.com>
2023-10-03 18:16:39 +0530

    logic part dice

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +10,56 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  AssetImage one = AssetImage("images/one.png");
+  AssetImage two = AssetImage("images/two.png");
+  AssetImage three = AssetImage("images/three.png");
+  AssetImage four = AssetImage("images/four.png");
+  AssetImage five = AssetImage("images/five.png");
+  AssetImage six = AssetImage("images/six.png");
+
+  AssetImage diceimage;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      diceimage = one;
+    });
+  }
+
+  void rollDice() {
+    int random = (1 + Random().nextInt(6));
+
+    AssetImage newImage;
+
+    switch (random) {
+      case 1:
+        newImage = one;
+        break;
+      case 2:
+        newImage = two;
+        break;
+      case 3:
+        newImage = three;
+        break;
+      case 4:
+        newImage = four;
+        break;
+      case 5:
+        newImage = five;
+        break;
+      case 6:
+        newImage = six;
+        break;
+      default:
+    }
+
+    setState(() {
+      diceimage = newImage;
+      
+    });
+  }
+
   Widget build(BuildContext context) {
     return Container();
   }
